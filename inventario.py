a21c6bac6acd3b3f9ee409c23b0651c11828cec3
pedrotokar <pedrotokar2004@gmail.com>
2023-10-30 22:12:59 -0300

    Coloquei mensagens falando o que aconteceu

--- inventario.py
+++ inventario.py
@@ -11,15 +11,16 @@ class Inventario:
             self.estoque[codigo_de_barras] = quantidade
         else:
             self.estoque[codigo_de_barras] += quantidade
+        print(f"Adicionado {quantidade} {produto.nome} no estoque.")
 
     def vender_produto(self, produto, quantidade):
         codigo_de_barras = produto.obter_codigo_de_barras()
         
         if codigo_de_barras not in self.estoque:
-                raise exc.ProdutoNaoDisponivel
+                raise exc.ProdutoNaoDisponivel(f"O produto {produto.nome} não existe no estoque")
         
         elif self.estoque[codigo_de_barras] < quantidade:
-                raise exc.QuantidadeNaoDisponivel
+                raise exc.QuantidadeNaoDisponivel(f"Não existem {quantidade} {produto.nome} no estoque, apenas {self.estoque[codigo_de_barras]}")
             
         else:
             self.estoque[codigo_de_barras] -= quantidade
