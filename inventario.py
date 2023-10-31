877ed0f90171fabfc62b1c0c8c3785088fe05fa2
GuilhermeCastilho02 <guilherme222castilho@gmail.com>
2023-10-30 22:03:45 -0300

    criei a venda d produto

--- inventario.py
+++ inventario.py
@@ -1,3 +1,5 @@
+import excecoes as exc
+
 class Inventario:
     def __init__(self, nome):
         self.nome = nome
@@ -9,3 +11,17 @@ class Inventario:
             self.estoque[codigo_de_barras] = quantidade
         else:
             self.estoque[codigo_de_barras] += quantidade
+
+    def vender_produto(self, produto, quantidade):
+        codigo_de_barras = produto.obter_codigo_de_barras()
+        
+        if codigo_de_barras not in self.estoque:
+                raise exc.ProdutoNaoDisponivel
+        
+        elif self.estoque[codigo_de_barras] < quantidade:
+                raise exc.QuantidadeNaoDisponivel
+            
+        else:
+            self.estoque[codigo_de_barras] -= quantidade
+            print(f"O {produto} foi vendido!")
+        
