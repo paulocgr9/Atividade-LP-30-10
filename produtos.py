8481dbd6f03807dffc81e9b13ce70749fcca03bd
pedrotokar <pedrotokar2004@gmail.com>
2023-10-30 22:35:38 -0300

    Adicionei funções especificas por porduto

--- produtos.py
+++ produtos.py
@@ -14,6 +14,9 @@ class Sabão(Produto):
     def obter_codigo_de_barras(self):
         return "789" + "600370516" + self.marca.value
 
+    def tomar_banho(self):
+        print("Você tomou banho")
+
 class Absorvente(Produto):
     def __init__(self, nome, marca, preco):
         super().__init__(nome, marca, preco)
@@ -21,9 +24,15 @@ class Absorvente(Produto):
     def obter_codigo_de_barras(self):
         return "789" + "023285809" + self.marca.value
 
+    def menstruar(self):
+        print("O absorvente segurou o seu sangue")
+
 class EscovaDeDente(Produto):
     def __init__(self, nome, marca, preco):
         super().__init__(self, nome, marca, preco)
     
     def obter_codigo_de_barras(self):
         return "789" + "677564728" + self.marca.value
+
+    def escovar_o_dente(self):
+        print("Sua boca não é mais um bueiro")
